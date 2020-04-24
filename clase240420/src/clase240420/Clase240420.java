29c6d4cde188256767c26415d0605e56ada46080
Freddy <Freddy@PC_LIMA>
2020-04-23 20:06:35 -0500

    He agregado una impresion por consola.

---
+++ clase240420/src/clase240420/Clase240420.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */ 
+package clase240420;
+
+/**
+ *
+ * @author Freddy
+ */
+public class Clase240420 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
