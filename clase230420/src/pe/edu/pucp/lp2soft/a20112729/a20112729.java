ec94f02c050dde107cbadf7d7fa3a4d37b787197
Freddy <Freddy@PC_LIMA>
2020-04-23 23:36:36 -0500

    Programacion

--- clase230420/src/pe/edu/pucp/lp2soft/a20112729/a20112729.java
+++ clase230420/src/pe/edu/pucp/lp2soft/a20112729/a20112729.java
@@ -3,6 +3,6 @@ package pe.edu.pucp.lp2soft.a20112729;
 public class a20112729 {
     public static void main(String[] args){
         System.out.println("Hola mundo cruel");
-        System.out.println("Joder");
+        System.out.println("AAAAAAA");
     }
 }
