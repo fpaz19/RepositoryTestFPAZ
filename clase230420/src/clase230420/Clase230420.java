9ebff3bf230a8bf1593cac537bb99b2eb710e7e1
Freddy <Freddy@PC_LIMA>
2020-04-23 23:13:06 -0500

    He agregado unas operaciones matematicas

--- clase230420/src/clase230420/Clase230420.java
+++ clase230420/src/clase230420/Clase230420.java
@@ -1,7 +1,5 @@
 package clase230420;
-
 public class Clase230420 {
-
     public static void main(String[] args) {
         System.out.println("He creado una nueva rama....");
         System.out.println("Hola como estas???");
@@ -9,5 +7,4 @@ public class Clase230420 {
         int b = 50 + 10;
         System.out.println(a + b);
     }
-    
 }
