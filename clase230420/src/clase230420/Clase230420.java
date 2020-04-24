1d5de1beeede0ced875477fdc0d996d26dd0309f
Freddy <Freddy@PC_LIMA>
2020-04-23 20:44:54 -0500

    He agregado una impresion por consola.

--- clase230420/src/clase230420/Clase230420.java
+++ clase230420/src/clase230420/Clase230420.java
@@ -4,6 +4,7 @@ public class Clase230420 {
 
     public static void main(String[] args) {
         System.out.println("He creado una nueva rama....");
+        System.out.println("Hola como estas???");
     }
     
 }
