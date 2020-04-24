21921a6c811979bef9b36961f1db2bd5d4ff1895
Freddy <Freddy@PC_LIMA>
2020-04-23 20:16:19 -0500

    He agregado una impresion por consola.

--- clase230420/src/clase230420/Clase230420.java
+++ clase230420/src/clase230420/Clase230420.java
@@ -3,7 +3,7 @@ package clase230420;
 public class Clase230420 {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("He creado una nueva rama....");
     }
     
 }
